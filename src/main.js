855a6df8226072869a97a11908967d419f73ebae
hopperhub <hoppergoo@gmail.com>
2018-08-17 11:05:51 +0800

    feature: 导入iVew; chore: babel-plugin-import

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css' // 导入 iView 样式
 
 Vue.config.productionTip = false
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
