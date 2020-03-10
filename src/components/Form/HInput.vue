890431b772bb0953732f6f1c436676ceeb3a4f9a
戴赠记 <hopper@daizengjideMacBook-Pro.local>
2020-03-10 23:30:54 +0800

    feat: 创建 form input 自定义组件

---
+++ src/components/Form/HInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input :type="type" :value="value" @input="input" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HInput', // input 组件
+  model: {
+    // 组件上的 v-model 会默认使用名为 value 的 prop 和名为 input 的 event 事件
+    prop: 'value',
+    event: 'input'
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    input ({ target: { value } }) {
+        this.$emit('input', value);
+    }
+  }
+}
+</script>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="wrap">
     <Header></Header>
-    <div class="body"></div>
+    <div class="body">
+      <h-input v-model="inputVal" />
+			<div>{{inputVal}}</div>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './Header/index'
+import HInput from './Form/HInput'
 
 export default {
   name: 'Index',
-  components: { Header }
+  components: {
+    Header,
+    HInput
+  },
+  data () {
+    return {
+      inputVal: '123'
+    }
+  }
 }
 </script>
 
